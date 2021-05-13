15ce8c9dd82498efd0467f8c36e027673ccd5256
gradi kayamba <gradikay>
2021-05-13 08:54:35 -0400

    my fisrt crypto todo list

---
+++ contracts/TodoList.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+contract TodoList{
+
+	// Uint variable taskcount
+	uint public taskcount;
+
+	// Assigning values to variables
+	constructor(){
+
+		taskcount = 0;
+	}
+
+}
